3562441511799aa153e23777cd1278a84cc5508b
Ninpoka <github.com@ninpoka.nl>
2025-01-14 18:43:58 +0100

    Simplified the InputText. No need for special event handlers anymore. also, made a beginning with data validation on input.

--- EbooksIndex.ClassLibrary/Models/SearchParameters.cs
+++ EbooksIndex.ClassLibrary/Models/SearchParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EbooksIndex.ClassLibrary.Models;
 
 public class SearchParameters
 {
+    [MinLength(4, ErrorMessage = "Zoek met 1 woord, minimaal 4 karakters lang")]
     public string? SingleSearchString { get; set; }
 
     public bool OnlyReference { get; set; } = false;
